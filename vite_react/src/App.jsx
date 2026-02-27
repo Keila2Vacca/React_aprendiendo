7eeb732577c9bb83e6e82a0bf85b6ed9d82a0e75
Keila2Vacca <knvaccab@ufpso.edu.co>
2026-02-27 08:52:48 -0500

    aprendiendo

---
+++ vite_react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Saludar from './playground/Component'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Keila Nathaly</h1>
+      <img class="logo" src="/Pascal.jpg" alt="Pascal" />
+      <Saludar/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ vite_react/src/playground/Component.jsx
@@ -0,0 +1,6 @@
+function Saludar(){
+    return(
+        <h2>Bienvenido a la app</h2>
+    )
+}
+ export default Saludar
