b4b90640ae870a11cf8cffbad2c4931de9e764d1
Keila2Vacca <knvaccab@ufpso.edu.co>
2026-04-17 08:49:33 -0500

    SCRUM-8 Autentificación co Google

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,3 +24,6 @@ export const auth = getAuth(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+
+ //exportar la autentificacón con Google
+export const googleProvider = new GoogleAuthProvider();
